19526d65cced2942303aabb87e482c60c177c949
TarekMoustafaElsayed <tarek.mostafa.elsayed.2003@gmail.com>
2025-10-17 16:37:26 +0300

    Added View Splash, Welcome View as well as Sign In View

---
+++ android/app/src/main/kotlin/com/nectar/nectar_groceries/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.nectar.nectar_groceries
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:nectar_groceries/view/splash_view.dart';
+
+import 'common/color_extension.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Online Groceries',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        fontFamily: "Gilroy",
+        colorScheme: ColorScheme.fromSeed(seedColor: TColor.primary),
+        useMaterial3: false,
+      ),
+      home: const SplashView(),
+    );
+  }
+}
